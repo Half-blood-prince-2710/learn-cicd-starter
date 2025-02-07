fed48a00651a16adfd6aeae6803fc3236b475445
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-07 16:15:01 +0530

    done

--- main.go
+++ main.go
@@ -97,8 +97,8 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func unused() {
-    // this function does nothing
-    // and is called nowhere
-}
+// func unused() {
+//     // this function does nothing
+//     // and is called nowhere
+// }
 
